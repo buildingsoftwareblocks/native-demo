f51992070aab48ea6cdc266e536e001b58834144
buildingsoftwareblocks <mark.wigmans@gmail.com>
2023-05-06 14:36:38 +0200

    first release

--- src/main/java/com/example/nativedemo/NativeDemoApplication.java
+++ src/main/java/com/example/nativedemo/NativeDemoApplication.java
@@ -3,6 +3,7 @@ package com.example.nativedemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableConfigServer
 @SpringBootApplication
 public class NativeDemoApplication {
 
